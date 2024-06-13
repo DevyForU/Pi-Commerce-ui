881eefff809aa618995367fc3239e6695a78ab74
Sullivan1301 <hei.sullivan.2@gmail.com>
2024-06-13 22:43:05 +0300

    feat: search bar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,73 +1,73 @@
 "use client";
 import { useEffect, useState } from "react";
-import BrandCard from "../../components/BrandCard"
-import CarCard from '../../components/CarCard';
+import BrandCard from "../../components/BrandCard";
+import CarCard from "../../components/CarCard";
 import Footer from "../../components/Footer";
-import NavBar from "../../components/NavBar"
+import NavBar from "../../components/NavBar";
 import { Car } from "../../interface/car";
 
+export default function LandingPage() {
+  const car: Car = {
+    name: "Audi R8 V10",
+    price: 230_000,
+    status: "Available",
+    type: "Diesel",
+    configuration: {
+      brand: "Audi",
+      model: "Coupé V10 GT RWD 2023",
+      color: "White",
+      engine: "5.2L V10",
+      place_number: 2,
+      power: 456,
+    },
+    imageList: [
+      { id: "1", url: "https://example.com/images/audi-r8-1.jpg" },
+      { id: "2", url: "https://example.com/images/audi-r8-2.jpg" },
+      { id: "3", url: "https://example.com/images/audi-r8-3.jpg" },
+    ],
+  };
 
-export default function landingPage() {
-    const car: Car = {
-        name: "Audi R8 V10",
-        price: 230_000,
-        status: "Available",
-        type: "Diesel",
-        configuration: {
-            brand: "Audi",
-            model: "Coupé V10 GT RWD 2023",
-            color: "White",
-            engine: "5.2L V10",
-            place_number: 2,
-            power: 456,
-        },
-        imageList: [
-            { id: "1", url: "https://example.com/images/audi-r8-1.jpg" },
-            { id: "2", url: "https://example.com/images/audi-r8-2.jpg" },
-            { id: "3", url: "https://example.com/images/audi-r8-3.jpg" }
-        ]
-    };
-
-    return (
-        <>
-        <div className="bg-[url('/car1.jpg')] bg-no-repeat bg-contain">
-            <div className="bg-black bg-opacity-65">
-                <NavBar/>
-                <div className="flex flex-col text-white items-center pt-40 pb-72">
-                    <p className="text-sm mb-2">Find cars for sale near you</p>
-                    <p className="font-semibold text-4xl">Find Your Perfect Car</p>
-                </div>
-                <div className="bg-slate-100 pt-16 pb-10 rounded-t-[70px]">
-                    <h1 className="text-2xl ml-44 mb-5 font-semibold">Explore Our Premium Brands</h1>
-                    <div className="flex justify-center gap-8">
-                        <BrandCard name="Mercedes Benz" icon="/mercedes-benz.svg" />
-                        <BrandCard name="Ford" icon="/ford.svg" />
-                        <BrandCard name="Renault" icon="/renault.svg" />
-                        <BrandCard name="Peugeot" icon="/peugeot.svg" />
-                        <BrandCard name="Volkswagen" icon="/volkswagen.svg" />
-                        <BrandCard name="Citroën" icon="/citroen.svg" />
-                    </div>
-                </div>
-                <div className="flex flex-col pt-12 pb-16 bg-white pl-44 overflow-x-auto">
-                    <h1 className="text-2xl mb-9  font-semibold">🔥 Popular cars</h1>
-                    <div className="flex gap-5 flex-wrap">
-                        <CarCard car={car} />
-                        <CarCard car={car} />
-                        <CarCard car={car} />
-                        <CarCard car={car} />
-                        <CarCard car={car} />
-                        <CarCard car={car} />
-                    </div>
-                </div>
-                <div>
-
-                </div>
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: 'url("/car1.jpg")',
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          minHeight: "90vh",
+        }}
+      >
+        <div className="bg-black bg-opacity-65">
+          <NavBar />
+          <div className="flex flex-col text-white items-center pt-40 pb-72">
+            <p className="text-sm mb-2">Find cars for sale near you</p>
+            <p className="font-semibold text-4xl">Find Your Perfect Car</p>
+          </div>
+          <div className="bg-slate-100 pt-16 pb-10 rounded-t-[70px]">
+            <h1 className="text-2xl ml-44 mb-5 font-semibold">Explore Our Premium Brands</h1>
+            <div className="flex justify-center gap-8">
+              <BrandCard name="Mercedes Benz" icon="/mercedes-benz.svg" />
+              <BrandCard name="Ford" icon="/ford.svg" />
+              <BrandCard name="Renault" icon="/renault.svg" />
+              <BrandCard name="Peugeot" icon="/peugeot.svg" />
+              <BrandCard name="Volkswagen" icon="/volkswagen.svg" />
+              <BrandCard name="Citroën" icon="/citroen.svg" />
             </div>
+          </div>
+          <div className="flex flex-col pt-12 pb-16 bg-white pl-44 overflow-x-auto">
+            <h1 className="text-2xl mb-9 font-semibold">🔥 Popular cars</h1>
+            <div className="flex gap-5 flex-wrap">
+              <CarCard car={car} />
+              <CarCard car={car} />
+              <CarCard car={car} />
+              <CarCard car={car} />
+              <CarCard car={car} />
+              <CarCard car={car} />
+            </div>
+          </div>
         </div>
-        <Footer></Footer>
-        </>
-        
-
-    )
+      </div>
+      <Footer />
+    </>
+  );
 }
-
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,34 +1,65 @@
-import  React  from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
+import React, { useState, ChangeEvent } from "react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
 
-export default function App() {
-    return (
-      <Navbar shouldHideOnScroll className="border-none shadow-md flex justify-between items-center px-10 pt-3 pb-14 sm:py-3 sm:pt-0 sm:pb-0">
-        <NavbarBrand className="grow">
-          <p className="text-2xl font-semibold italic text-white">Vaika Store</p>
-        </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-4 justify-center items-center">
-          <NavbarItem>
-            <Link color="foreground" href="/home" className="text-white">
-              Home
-            </Link>
-          </NavbarItem>
-          <NavbarItem isActive>
-            <Link href="#" aria-current="page" className="text-white">
-              Our Brands
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#" className="text-white">
-              Our Cars
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-        <NavbarContent justify="end">
-          <NavbarItem className="bg-slate-200 text-slate-800 px-2 py-2 rounded-lg shadow-slate-500 active:translate-y-[4px] active:shadow-none transition-all">
-            <Button href="/sign-in" > Login </Button>
-          </NavbarItem>
-        </NavbarContent>
-      </Navbar>
-    );
-  }
+export default function NavBar() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleResetSearch = () => {
+    setSearchValue("");
+  };
+
+  return (
+    <Navbar shouldHideOnScroll className="border-none shadow-md flex justify-between items-center px-10 pt-3 pb-14 sm:py-3 sm:pt-0 sm:pb-0">
+      <NavbarBrand className="grow">
+        <p className="text-2xl font-semibold italic text-white">Vaika Store</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4 justify-between items-center">
+        <NavbarItem>
+          <Link color="foreground" href="/home" className="text-white">
+            Home
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link href="#" aria-current="page" className="text-white">
+           Brands
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#" className="text-white">
+             Cars
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent className="flex items-center lg:shrink-0 lg:flex-nowrap">
+        <div className="relative flex items-center lg:ml-4 sm:mr-0 mr-2">
+          <span className="absolute ml-4 leading-none -translate-y-1/2 top-1/2 text-muted">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-slate-800">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+          </span>
+          <input
+            className="block w-full min-w-[50px] py-3 pl-12 pr-4 text-base font-medium leading-normal bg-slate-200 border border-solid outline-none appearance-none placeholder:text-secondary-dark peer text-slate-200 border-none bg-clip-padding rounded-xl"
+            placeholder="Search..."
+            type="text"
+            value={searchValue}
+            onChange={handleSearchChange}
+          />
+          <span onClick={handleSearchChange} className="absolute right-0 left-auto mr-4 leading-none -translate-y-1/2 peer-placeholder-shown:hidden top-1/2 hover:text-primary text-muted">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
+          </span>
+        </div>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="bg-slate-600 text-slate-200 px-2 py-2 rounded-lg shadow-slate-500 active:translate-y-[4px] active:shadow-none transition-all">
+          <Button href="/sign-in"> Login </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
